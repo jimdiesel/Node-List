bc8364755a716a23c8973b6df19185d37257aeb9
Jim Danko <nofxbassist1011@gmail.com>
2011-07-22 21:56:24 -0400

    Added blank login handler, updated routes in index to use it, and added formidable modules for easier form parsing.

---
+++ modules/login.js
@@ -0,0 +1,13 @@
+var querystring = require("querystring"),
+	formidable = require("formidable");
+
+function login(response, request) {
+
+}
+
+function showPage(response, request) {
+
+}
+
+exports.login = login;
+exports.showPage = showPage;
