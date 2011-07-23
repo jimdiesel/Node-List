713a3733cfc333a5506fbe77e388f13f6e646569
Jim Danko <nofxbassist1011@gmail.com>
2011-07-22 22:05:56 -0400

    Rolled login functions into user module, updated index to use new functions.

---
+++ modules/user.js
@@ -0,0 +1,33 @@
+var querystring = require("querystring"),
+	formidable = require("formidable");
+
+function login(response, request) {
+
+}
+
+function update(response, request) {
+
+}
+
+function select(response, request) {
+
+}
+
+function showPageLogin(response, request) {
+
+}
+
+function showPageUser(response, request) {
+
+}
+
+function showPageEdit(response, request) {
+
+}
+
+exports.login = login;
+exports.update = update;
+exports.select = select;
+exports.showPageLogin = showPageLogin;
+exports.showPageUser = showPageUser;
+exports.showPageEdit = showPageEdit;
