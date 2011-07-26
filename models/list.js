1988bc0f6a0440b049da48f17150c98dc2c949b8
Jim Danko <nofxbassist1011@gmail.com>
2011-07-25 22:19:26 -0400

    Added method to create list.

--- models/list.js
+++ models/list.js
@@ -1,11 +1,25 @@
 var base = require("../base");
 
-function create(list) {
-
+function create(list, callback) {
+	var client = base.init();
+	var values = [list.name, list.order];
+	client.query('INSERT INTO lists (name, order, created, modified) VALUES (?, ?, NOW(), NOW())', values, function(error, results) {
+		if (error) {
+			console.log("Error creating list: " + erorr.message);
+			client.end();
+			return;
+		}
+		console.log("Inserted: " + results.affectedRows + " row");
+		console.log("Id inserted: " + results.insertId);
+		list.id = results.insertId;
+	});
+	if (callback && typeof(callback) == "function") {
+		callback(list);
+	}
 }
 
 function update(list, callback) {
-
+	
 }
 
 function deleteById(id, callback) {
