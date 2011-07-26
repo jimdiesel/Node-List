a48899ff22974609d3a46c7b30568527dce19011
Jim Danko <nofxbassist1011@gmail.com>
2011-07-25 22:11:02 -0400

    Added model for lists.

---
+++ models/list.js
@@ -0,0 +1,22 @@
+var base = require("../base");
+
+function create(list) {
+
+}
+
+function update(list, callback) {
+
+}
+
+function deleteById(id, callback) {
+
+}
+
+function selectById(id, callback) {
+
+}
+
+exports.create = create;
+exports.update = update;
+exports.deleteById = deleteById;
+exports.selectById = selectById;
