0bc64f4934f379e5105ed784df211118708e8e39
Jim Danko <nofxbassist1011@gmail.com>
2011-07-25 22:43:20 -0400

    Added methods to delete list, select list by id, and select lists by user id.

--- models/list.js
+++ models/list.js
@@ -12,10 +12,10 @@ function create(list, callback) {
 		console.log("Inserted: " + results.affectedRows + " row");
 		console.log("Id inserted: " + results.insertId);
 		list.id = results.insertId;
+		if (callback && typeof(callback) == "function") {
+			callback(list);
+		}
 	});
-	if (callback && typeof(callback) == "function") {
-		callback(list);
-	}
 }
 
 function update(list, callback) {
@@ -28,21 +28,71 @@ function update(list, callback) {
 			return;
 		}
 		console.log("Updated: " + results.affectedRows + " row(s)");
+		if (callback && typeof(callback) == "function") {
+			callback(true);
+		}
 	});
-	if (callback && typeof(callback) == "function") {
-		callback(true);
-	}
 }
 
 function deleteById(id, callback) {
-
+	var client = base.init();
+	var values = [id];
+	// future enhancement:
+	// modify query to also delete tasks
+	// associated with the deleted list
+	client.query('DELETE FROM lists WHERE id = ?', values, function(error, results) {
+		if (error) {
+			console.log("Error deleting user: " + error.message);
+			client.end();
+			return;
+		}
+		console.log("Deleted: " + results.affectedRows + " row(s)");
+		if (callback && typeof(callback) == "function") {
+			callback(true);
+		}
+	});
 }
 
 function selectById(id, callback) {
+	var client = base.init();
+	var values = [id];
+	var list;
+	client.query('SELECT id,user_id,name,order,created,modified FROM lists WHERE id = ?', values, function(error, results) {
+		if (error) {
+			console.log("Error selecting list: " + error.message);
+			client.end();
+			return;
+		}
+		if (results.length > 0) {
+			list = results[0];
+		}
+		if (callback && typeof(callback) == "function") {
+			callback(list);
+		}
+	});
+}
 
+function selectByUserId(userId, callback) {
+	var client = base.init();
+	var values = [userId];
+	var lists;
+	client.query('SELECT id,user_id,name,order,created,modified FROM lists WHERE user_id = ?', values, function(error, results) {
+		if (error) {
+			console.log("Error selecting lists: " + error.message);
+			client.end();
+			return;
+		}
+		if (results.length > 0) {
+			lists = results;
+		}
+		if (callback && typeof(callback) == "function") {
+			callback(lists);
+		}
+	});
 }
 
 exports.create = create;
 exports.update = update;
 exports.deleteById = deleteById;
 exports.selectById = selectById;
+exports.selectByUserId = selectByUserId;
