6681fee2e14f87159247644e4eb320c7359c68c8
Jim Danko <nofxbassist1011@gmail.com>
2011-07-25 22:06:12 -0400

    Added controller for lists.

---
+++ controllers/list.js
@@ -0,0 +1,32 @@
+var listPage = require("../views/list/index");
+
+function create(response, request, pageData) {
+
+}
+
+function update(response, request, pageData) {
+
+}
+
+function deleteList(response, request, pageData) {
+
+}
+
+function showPageList(response, request) {
+
+}
+
+function showPageCreate(response, request) {
+
+}
+
+function showPageEdit(response, request) {
+
+}
+
+exports.create = create;
+exports.update = update;
+exports.deleteList = deleteList;
+exports.showPageList = showPageList;
+exports.showPageCreate = showPageCreate;
+exports.showPageEdit = showPageEdit;
