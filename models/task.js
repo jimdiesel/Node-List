9048e673b54f77b30450e9a9e9ca2d5a67f4f76f
Jim Danko <nofxbassist1011@gmail.com>
2011-09-04 20:12:25 -0400

    Added model for tasks table with blank functions.

---
+++ models/task.js
@@ -0,0 +1,32 @@
+var base = require("./base");
+
+function create(task, callback) {
+
+}
+
+function update(task, callback) {
+
+}
+
+function deleteById(id, callback) {
+
+}
+
+function deleteByListId(listId, callback) {
+
+}
+
+function selectById(id, callback) {
+
+}
+
+function selectByListId(listId, callback) {
+
+}
+
+exports.create = create;
+exports.update = update;
+exports.deleteById = deleteById;
+exports.deleteByListId = deleteByListId;
+exports.selectById = selectById;
+exports.selectByListId = selectByListId;
