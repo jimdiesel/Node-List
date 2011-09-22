82556566f66df245bfa7179afc1167c0885281a1
Jim Danko <nofxbassist1011@gmail.com>
2011-09-22 01:24:39 -0400

    List detail page now checks for a null task list.

--- views/list/detail.js
+++ views/list/detail.js
@@ -5,42 +5,48 @@ function build(response, request, pageData, list, tasks) {
 	html = html + base.openPage(pageData);
 	html = html +
 		'<a href="/lists">&laquo; Back</a>'+
-		'<h1>Tasks for ' + list.name + '</h1>'+
-		'<p><a href="/lists/' + list.id + '/add">Add a Task</a></p>'+
-		'<form action="/lists/' + list.id + '" method="post">'+
-		'<h2>Incomplete Tasks</h2>'+
-		'<ul>';
-	for (var i = 0;i < tasks.length;i++) {
-		if (tasks[i].complete == '0') {
-			html = html +
-				'<li>'+
-				'<label>'+
-				'<input type="checkbox" name="' + tasks[i].id + '" />'+
-				' <a href="/lists/' + list.id + '/' + tasks[i].id + '">' + tasks[i].name + '</a>'+
-				'</label>'+
-				'</li>';
+		'<h1>Tasks for ' + list.name + '</h1>';
+	if (tasks != 'undefined' && tasks != null) {
+		html = html +
+			'<p><a href="/lists/' + list.id + '/add">Add a Task</a></p>'+
+			'<form action="/lists/' + list.id + '" method="post">'+
+			'<h2>Incomplete Tasks</h2>'+
+			'<ul>';
+		for (var i = 0;i < tasks.length;i++) {
+			if (tasks[i].complete == '0') {
+				html = html +
+					'<li>'+
+					'<label>'+
+					'<input type="checkbox" name="' + tasks[i].id + '" />'+
+					' <a href="/lists/' + list.id + '/' + tasks[i].id + '">' + tasks[i].name + '</a>'+
+					'</label>'+
+					'</li>';
+			}
 		}
-	}
-	html = html +
-		'</ul>'+
-		'<h2>Completed Tasks</h2>'+
-		'<ul>';
-	for (var i = 0;i < tasks.length;i++) {
-		if (tasks[i].complete == '1') {
-			html = html +
-				'<li>'+
-				'<label>'+
-				'<input type="hidden" name="' + tasks[i].id + '" value="0" />'+
-				'<input type="checkbox" name="' + tasks[i].id + '" checked="checked" />'+
-				' <a href="/lists/' + list.id + '/' + tasks[i].id + '">' + tasks[i].name + '</a>'+
-				'</label>'+
-				'</li>';
+		html = html +
+			'</ul>'+
+			'<h2>Completed Tasks</h2>'+
+			'<ul>';
+		for (var i = 0;i < tasks.length;i++) {
+			if (tasks[i].complete == '1') {
+				html = html +
+					'<li>'+
+					'<label>'+
+					'<input type="hidden" name="' + tasks[i].id + '" value="0" />'+
+					'<input type="checkbox" name="' + tasks[i].id + '" checked="checked" />'+
+					' <a href="/lists/' + list.id + '/' + tasks[i].id + '">' + tasks[i].name + '</a>'+
+					'</label>'+
+					'</li>';
+			}
 		}
+		html = html +
+			'</ul>'+
+			'<input type="submit" value="Update List" />'+
+			'</form>';
+	} else {
+		html = html +
+		'<p>There are no tasks for this list. Why not <a href="/lists/' + list.id + '/add">add a task</a>?</p>';
 	}
-	html = html +
-		'</ul>'+
-		'<input type="submit" value="Update List" />'+
-		'</form>';
 	html = html + base.closePage(pageData);
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write(html);
