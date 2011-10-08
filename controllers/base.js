b75931974eb5745dcc7c678db340274ab8adf839
Jim Danko <nofxbassist1011@gmail.com>
2011-10-08 19:45:02 -0400

    Added helper method for user login validation.

--- controllers/base.js
+++ controllers/base.js
@@ -5,4 +5,25 @@ function PageData() {
 	this.message = '';
 }
 
+function validateUser(request, response, id, callback) {
+	if (request.session.data.user != null && request.session.data.user != '' && request.session.data.user != 'undefined') {
+		userDb.selectById(request.session.data.user, function(user) {
+			if (user == null || user == 'undefined') {
+				redirectToLogin(response);
+			} else {
+				if (callback && typeof(callback) == 'function') {
+					callback();
+				}
+			}
+		});
+	} else {
+		redirectToLogin(response);
+	}
+}
+
+function redirectToLogin(response) {
+	response.writeHead(302, {"Location": "/user/login"});
+	response.end();
+}
+
 exports.PageData = PageData;
