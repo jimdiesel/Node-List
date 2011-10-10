d65d14618d0442ec0a705e45e603df7161c75ea0
Jim Danko <nofxbassist1011@gmail.com>
2011-10-09 22:15:51 -0400

    Added markup for list delete page.

--- views/list/delete.js
+++ views/list/delete.js
@@ -1,6 +1,16 @@
 var base = require("..base");
 
-function build(response, request, pageData) {
+function build(response, request, pageData, list) {
+	var html = '';
+	html = html + base.openPage(pageData);
+	html = html +
+		'<h1>Delete List</h1>'+
+		'<p><strong>Are you sure you want to delete ' + list.name + '?</strong></p>'+
+		'<p><a href="/lists/' + list.id + '/delete">Yes</a> <a href="/lists">No</a></p>';
+	html = html + base.closePage(pageData);
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(html);
+	response.end();
 }
 
 exports.build = build;
