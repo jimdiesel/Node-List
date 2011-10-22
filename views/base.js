078b959113e5148fdc3df62b5690724427d38797
Jim Danko <nofxbassist1011@gmail.com>
2011-10-21 23:56:02 -0400

    Added markup and styles for top navigation.

--- views/base.js
+++ views/base.js
@@ -9,13 +9,28 @@ function openPage(pageData) {
 		'<meta name="keywords" content="' + pageData.metaKeywords + '" />'+
 		'<link rel="stylesheet" media="screen" href="/_assets/css/site.css" />'+
 		'<link rel="shortcut icon" href="/_assets/img/favicon.ico" />'+
-		'<body>';
+		'<body>'+
+		'<div id="container">'+
+		'<header>'+
+		'<h1>Node List</h1>'+
+		'<nav>'+
+		'<a href="/">Home</a>'+
+		'<a href="/lists">Lists</a>'+
+		'<a href="/user/edit">Account</a>'+
+		'<a href="/user/logout">Log Out</a>'+
+		'</nav>'+
+		'</header>'+
+		'<section id="content">';
 	return html;
 }
 
 function closePage(pageData) {
 	var html = ''+
+		'</section>'+
+		'</div>'+
+		'<footer>'+
 		'<script type="text/javascript" src="/_assets/js/jquery.js"></script>'+
+		'</footer>'+
 		'</body>'+
 		'</html>';
 	return html;
